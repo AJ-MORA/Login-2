1679f0bf490fa6c57bf275d10888bd591cd6165f
Andres Mora <javiermora.dussan@gmail.com>
2023-07-10 14:55:17 -0500

    autenticar_usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -23,6 +23,7 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from './user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+interface User {
+  username: string;
+  password: string;
+}
 @Component({
   // selector: 'app-login',
   templateUrl: './login.component.html',
@@ -17,37 +21,40 @@ export class LoginComponent {
   ];
 
   newUser: User = { username: '', password: ''};
+  loginForm: FormGroup;
 
-  constructor(private router: Router) {}
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   login() {
-    console.log('Iniciando sesión con el usuario:', this.newUser.username);
+    const { username, password } = this.loginForm.value;
     const matchedUser = this.users.find(
-      user =>
-      user.username === this.newUser.username &&
-      user.password === this.newUser.password
-      );
+      user => user.username === username && user.password === password
+    );
     if (matchedUser) {
       console.log('Usuario encontrado en nuestra BD:', matchedUser);
-      //Autenticacion exitosa, redireccionar a la pagina principal
+      // Autenticacion exitosa, redireccionar a la pagina principal
       this.router.navigateByUrl('/home');
     } else {
       console.log('ERROR!!Este Usuario NO se encuentra en Nuestra BD');
-      //Mostrar un mensaje de error
+      // Mostrar un mensaje de error
       alert('Nombre de usuario o contraseña incorrectos');
     }
   }
 
   createUser() {
-    //Verificar si el nuevo usuario ya existe
-    const existingUser = this.users.find(
-      (user) => user.username === this.newUser.username
-    );
+    const { username, password } = this.loginForm.value;
+    // Verificar si el nuevo usuario ya existe
+    const existingUser = this.users.find(user => user.username === username);
     if (existingUser) {
-      alert ('El nombre de usuario ya existe en nuestra BD');
+      alert('El nombre de usuario ya existe en nuestra BD');
     } else {
-      //Agregar el nuevo usuario a la lista
-      this.users.push(this.newUser);
+      // Agregar el nuevo usuario a la lista
+      this.users.push({ username, password });
       alert('¡¡Usuario creado exitosamente!!');
     }
   }
